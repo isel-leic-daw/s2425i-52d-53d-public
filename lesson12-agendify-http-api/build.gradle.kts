1e808e4054ba0ad2c8064ff6550db67300c407c7
Miguel Gamboa <miguelgamboa@outlook.com>
2024-10-10 10:06:30 +0100

    Refactor docker-compose and make tests depending on JDBI to startup, wait and shutdown Postgres Container

--- lesson12-agendify-http-api/build.gradle.kts
+++ lesson12-agendify-http-api/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
+    id("com.adarshr.test-logger") version "4.0.0"
 }
 
 group = "pt.isel"
@@ -44,4 +45,29 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    dependsOn(":lesson13-agendify-repository-jdbi:dbTestsWait")
+    finalizedBy(":lesson13-agendify-repository-jdbi:dbTestsDown")
+}
+
+/**
+ * DB related tasks
+ * - To run `psql` inside the container, do
+ *      docker exec -ti db-tests psql -d db -U dbuser -W
+ *   and provide it with the same password as define on `tests/Dockerfile-db-test`
+ */
+
+val composeFileDir: Directory = rootProject.layout.projectDirectory
+val dockerComposePath = composeFileDir.file("docker-compose.yml").toString()
+
+task<Exec>("dbTestsUp") {
+    commandLine("docker", "compose", "-f", dockerComposePath, "up", "-d", "--build", "--force-recreate", "db-tests")
+}
+
+task<Exec>("dbTestsWait") {
+    commandLine("docker", "exec", "db-tests", "/app/bin/wait-for-postgres.sh", "localhost")
+    dependsOn("dbTestsUp")
+}
+
+task<Exec>("dbTestsDown") {
+    commandLine("docker", "compose", "-f", dockerComposePath, "down", "db-tests")
 }
--- lesson13-agendify-repository-jdbi/build.gradle.kts
+++ lesson13-agendify-repository-jdbi/build.gradle.kts
@@ -24,7 +24,32 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    dependsOn(":lesson13-agendify-repository-jdbi:dbTestsWait")
+    finalizedBy(":lesson13-agendify-repository-jdbi:dbTestsDown")
 }
 kotlin {
     jvmToolchain(21)
+}
+
+/**
+ * DB related tasks
+ * - To run `psql` inside the container, do
+ *      docker exec -ti db-tests psql -d db -U dbuser -W
+ *   and provide it with the same password as define on `tests/Dockerfile-db-test`
+ */
+
+val composeFileDir: Directory = rootProject.layout.projectDirectory
+val dockerComposePath = composeFileDir.file("docker-compose.yml").toString()
+
+task<Exec>("dbTestsUp") {
+    commandLine("docker", "compose", "-f", dockerComposePath, "up", "-d", "--build", "--force-recreate", "db-tests")
+}
+
+task<Exec>("dbTestsWait") {
+    commandLine("docker", "exec", "db-tests", "/app/bin/wait-for-postgres.sh", "localhost")
+    dependsOn("dbTestsUp")
+}
+
+task<Exec>("dbTestsDown") {
+    commandLine("docker", "compose", "-f", dockerComposePath, "down", "db-tests")
 }
