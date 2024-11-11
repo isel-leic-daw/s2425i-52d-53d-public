2b8c62a1865d96f9f09d9ca0607df3f64cce65f2
Miguel Gamboa <miguelgamboa@outlook.com>
2024-11-11 10:00:14 +0000

    Lesson 27 Setup

---
+++ lesson27-react-state/src/demo06-textfield-validator.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom/client'
+
+const root = ReactDom.createRoot(document.getElementById('container'))
+
+export function demo06() {
+    root.render(
+    )
+}
---
+++ lesson27-react-state/src/index.ts
@@ -0,0 +1,7 @@
+// import { demo01 } from "./demo01-intro-react"
+// import { demo02 } from "./demo02-reconciliation"
+// import { demo03 } from "./demo03-components"
+// import { demo04 } from "./demo04-counter-state-WRONG";
+import { demo06 } from "./demo06-textfield-validator";
+
+demo06()
