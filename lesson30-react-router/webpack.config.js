329f0860ce9818f5199ab1c402c82aa146205bde
Miguel Gamboa <miguelgamboa@outlook.com>
2024-11-19 12:15:07 +0000

    Lesson 30 - React router. Proxy to backend

--- lesson30-react-router/webpack.config.js
+++ lesson30-react-router/webpack.config.js
@@ -3,6 +3,17 @@ const path = require('path');
 module.exports = {
   entry: './src/index.ts',
   mode: 'development',
+  devServer: {
+    port: 8000,
+    historyApiFallback: true,
+    compress: false,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:8080',
+      },
+    ],
+  },
   module: {
     rules: [
       {
